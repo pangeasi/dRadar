35b3ad9956050b409641fa33d0caa37f0ea9a99f
Dani <info@daniballesteros.es>
2019-12-18 19:35:53 +0100

    fetch node

--- functions/tmdb-multi-search.js
+++ functions/tmdb-multi-search.js
@@ -1,3 +1,5 @@
+const fetch = require("node-fetch");
+
 exports.handler = async (event, context) => {
   const result = await fetch(
     `https://api.themoviedb.org/3/search/multi?api_key=e227fe5710a79491de90085098092028&language=en-US&query=dragon%20ball&page=1&include_adult=false`
