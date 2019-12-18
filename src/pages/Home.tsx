f850467ff2950c95c991ef6e3b8dd090b8bfe4b9
Dani <info@daniballesteros.es>
2019-12-18 17:21:23 +0100

    create user front

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 import "./Home.css";
 import { Layout } from "../components/UI/Layout";
+import { IonButton } from "@ionic/react";
 
 const HomePage: React.FC = () => {
-  return <Layout title="Home">home</Layout>;
+  const create = (data: any) => {
+    return fetch("/.netlify/functions/add-user", {
+      body: JSON.stringify(data),
+      method: "POST"
+    }).then(response => {
+      return response.json();
+    });
+  };
+  return (
+    <Layout title="Home">
+      <IonButton onClick={() => create({ user: "fitopato.id.blockstack" })}>
+        create-user
+      </IonButton>
+    </Layout>
+  );
 };
 
 export default HomePage;
